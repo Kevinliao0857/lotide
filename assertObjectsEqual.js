9e741ec128f7562a1f75defd921aabe504b8bec6
Kevinliao0857 <kevinliao0857@gmail.com>
2021-02-04 03:57:53 +0000

    asobjeq upgrades

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,13 +1,8 @@
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
   console.log(object1, object2)
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (let key of Object.keys(object1)) {
       if(Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (!eqArrays(object1[key], object2[key])){
-          return false;
-        }
       } else if (object1[key] !== object2[key]) {
         return false
       }
@@ -15,21 +10,6 @@ const eqObjects = function(object1, object2) {
     } else {
     return false;
   }
-  return true;
-};
-
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-        if ((arr1[i] !== arr2[i])) {
-          return false;
-      }
-    }
-  } else {
-    return false;
-  }
-
   return true;
 };
 
