ebf88ab1fa3a4c23178eeb8027ec2f09b4c1840d
Kevinliao0857 <kevinliao0857@gmail.com>
2021-02-05 01:16:48 +0000

    upgrades

---
+++ map.js
@@ -0,0 +1,37 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+        if ((arr1[i] !== arr2[i])) {
+          return false;
+      }
+    }
+  } else {
+    return false;
+  }
+
+  return true;
+};
+
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log("✅✅✅ Assertion Passed: Arrays are equal.");  
+  } else {
+      console.log("🛑🛑🛑 Assertion Failed: Arrays are unequal.");
+  }
+};
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const words = ["up", "down", "left", "right"];
+
+const results1 = map(words, (word) => "fly " + word);
+
+assertArraysEqual(results1, ["fly up", "fly down", "fly left", "fly right"]);
